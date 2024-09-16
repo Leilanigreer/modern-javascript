75a1532c1de8ea16fb64ee7f48c060891c360932
Leilanigreer <leilani@lpcgolf.com>
2024-09-16 16:52:28 -0700

    added moment through html file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+/* global moment */
+
 console.log("hello world");
+
+var datetime = moment().startOf("day").fromNow();
+console.log(datetime);
+
+console.log(moment().calendar());
+
+console.log(moment("20190608", "YYYYMMDD").fromNow());
+// console.log(moment("06082019", "MMDDYYYY").fromNow().format('llll'));
+console.log(moment.format('llll'));
