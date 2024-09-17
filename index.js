8bc9212e457b90ade7046cf9e380328825e2ae70
Leilanigreer <leilani@lpcgolf.com>
2024-09-16 18:38:41 -0700

    added babble, created webpack config and tested some ES2015 (ES6)

--- index.js
+++ index.js
@@ -16,4 +16,40 @@ console.log(moment("20190608", "YYYYMMDD").fromNow());
 
 console.log(moment("06082019", "MMDDYYYY").format('MMMM Do YYYY'));
 
+let fibonacci = {
+  [Symbol.iterator]() {
+    let pre = 0, cur = 1;
+    return {
+      next() {
+        [pre, cur] = [cur, pre + cur];
+        return { done: false, value: cur }
+      }
+    }
+  }
+}
+
+for (var n of fibonacci) {
+  // truncate the sequence at 1000
+  if (n > 1000)
+    break;
+  console.log(n);
+}
+
+
+function f() {
+  {
+    let x;
+    {
+      // this is ok since it's a block scoped name
+      const x = "sneaky";
+      // error, was just defined with `const` above
+      x = "foo";
+    }
+    // this is ok since it was declared with `let`
+    x = "bar";
+    // error, already declared above in this block
+    // let x = "inner";
+  }
+}
+
 
